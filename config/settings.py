322ad8a205c961b0a7f09acb4445fa7a08405253
vinaydheeraj1234 <vinaydheerajpakki@gmail.com>
2026-09-15 22:44:43 +0530

    added swagger and error occured

--- config/settings.py
+++ config/settings.py
@@ -10,22 +10,25 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR / ".env")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e&13x66pnv&r_xq!%^^6o65h4ky^t-u6v5^r8evewj$i+1gizz'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -37,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'drf_yasg',
+    'authentication',
 ]
 
 MIDDLEWARE = [
@@ -124,4 +131,10 @@ MAILERS = {
     'default': {
         'BACKEND': 'django.core.mail.backends.console.EmailBackend',
     },
+}
+
+# Swagger
+SWAGGER_SETTINGS = {
+    "USE_SESSION_AUTH": False,  
+    "SECURITY_DEFINITIONS": None,  
 }
